ab7d08de9fdd5ef23359d4cb57294412b7d14017
a13669586274 <13669586274@qq.com>
2018-04-16 00:12:23 +0800

    car_system

---
+++ car_system/db_car_system.sql
@@ -0,0 +1,147 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50553
+Source Host           : localhost:3306
+Source Database       : db_car_system
+
+Target Server Type    : MYSQL
+Target Server Version : 50553
+File Encoding         : 65001
+
+Date: 2018-04-16 00:09:20
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `t_admin`;
+CREATE TABLE `t_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `account` char(20) NOT NULL COMMENT '超级管理员账号',
+  `password` char(32) NOT NULL COMMENT '超级管理员密码，md5加密',
+  `create_time` char(19) NOT NULL COMMENT '创建记录时间',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_admin
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_car
+-- ----------------------------
+DROP TABLE IF EXISTS `t_car`;
+CREATE TABLE `t_car` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `number` char(20) NOT NULL COMMENT '车辆编号',
+  `plate` char(7) NOT NULL COMMENT '车牌号',
+  `frame` char(17) NOT NULL COMMENT '车架号，VIN码17位',
+  `create_time` char(19) NOT NULL COMMENT '创建记录时间',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_car
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_driver
+-- ----------------------------
+DROP TABLE IF EXISTS `t_driver`;
+CREATE TABLE `t_driver` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `number` char(20) NOT NULL COMMENT '司机编号',
+  `account` char(20) NOT NULL COMMENT '司机账号',
+  `password` char(32) NOT NULL COMMENT '司机密码',
+  `name` char(20) NOT NULL COMMENT '真实姓名',
+  `image` char(30) DEFAULT NULL COMMENT '头像路径',
+  `create_time` char(19) NOT NULL COMMENT '创建记录时间',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_driver
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_driver_car
+-- ----------------------------
+DROP TABLE IF EXISTS `t_driver_car`;
+CREATE TABLE `t_driver_car` (
+  `driver_id` int(11) NOT NULL COMMENT '司机ID',
+  `car_id` int(11) NOT NULL COMMENT '车辆ID',
+  `create_time` char(19) NOT NULL COMMENT '创建记录时间',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
+  PRIMARY KEY (`driver_id`,`car_id`),
+  UNIQUE KEY `car_id` (`car_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_driver_car
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_goods
+-- ----------------------------
+DROP TABLE IF EXISTS `t_goods`;
+CREATE TABLE `t_goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `number` char(20) NOT NULL COMMENT '商品编号',
+  `name` char(20) NOT NULL COMMENT '商品名称',
+  `quantity` char(10) NOT NULL COMMENT '商品数量',
+  `real_quantity` char(10) NOT NULL COMMENT '商品实际数量',
+  `create_time` char(19) NOT NULL COMMENT '创建记录时间',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `number` char(20) NOT NULL COMMENT '订单编号',
+  `order_number` char(20) DEFAULT NULL COMMENT '订单号',
+  `goods_name` char(20) NOT NULL COMMENT '商品名称',
+  `goods_quantity` char(10) NOT NULL COMMENT '商品数量',
+  `create_time` char(19) NOT NULL COMMENT '订单创建时间',
+  `departure_time` char(16) DEFAULT NULL COMMENT '订单出发时间',
+  `car_plate` char(7) DEFAULT NULL COMMENT '车牌号',
+  `destination` char(30) NOT NULL COMMENT '目的地',
+  `order_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '订单状态，0表示未接单，1表示已接单，2表示已结束',
+  `driver_number` char(20) DEFAULT NULL COMMENT '司机编号',
+  `pick_number` char(20) DEFAULT NULL COMMENT '提货单号',
+  `contract_number` char(20) DEFAULT NULL COMMENT '合同号',
+  `short_info` char(20) DEFAULT NULL COMMENT '缺货信息',
+  `pick_quantity` char(10) DEFAULT NULL COMMENT '商品提货数量',
+  `pick_time` char(16) DEFAULT NULL COMMENT '提货时间',
+  `company` char(20) DEFAULT NULL COMMENT '结算单位',
+  `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
+  `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_order
+-- ----------------------------
