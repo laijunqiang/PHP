6736ba84de620be9a8d312435d967999d9e292f5
““laijunqian <2625407061@qq.com>
2018-05-16 21:58:19 +0800

    车辆预约系统--后台

--- vehicle_reservation_system/db_vehicle_reservation_system.sql
+++ vehicle_reservation_system/db_vehicle_reservation_system.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50553
 File Encoding         : 65001
 
-Date: 2018-05-14 22:12:02
+Date: 2018-05-16 21:58:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -73,7 +73,7 @@ CREATE TABLE `t_log` (
   `delete_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
   `status` tinyint(4) DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_log
@@ -100,6 +100,22 @@ INSERT INTO `t_log` VALUES ('19', 'admin', '生成账号为：13669586274 的用
 INSERT INTO `t_log` VALUES ('20', 'admin', '修改用户ID为：6 的用户', '2018-05-14 21:44:09', null, '0');
 INSERT INTO `t_log` VALUES ('21', 'admin', '生成账号为：13669586666 的用户', '2018-05-14 21:44:46', null, '0');
 INSERT INTO `t_log` VALUES ('22', 'admin', '删除用户ID为：6 的用户', '2018-05-14 21:47:06', null, '0');
+INSERT INTO `t_log` VALUES ('23', 'admin', '修改权限ID为：31 的权限', '2018-05-16 18:43:26', null, '0');
+INSERT INTO `t_log` VALUES ('24', 'admin', '生成公告编号为：N6018 的公告', '2018-05-16 19:25:36', null, '0');
+INSERT INTO `t_log` VALUES ('25', 'admin', '删除公告ID为：2 的公告', '2018-05-16 19:30:55', null, '0');
+INSERT INTO `t_log` VALUES ('26', 'admin', '修改公告编号为：N6018 的公告', '2018-05-16 19:38:56', null, '0');
+INSERT INTO `t_log` VALUES ('27', 'admin', '生成公告编号为：N5565 的公告', '2018-05-16 19:45:41', null, '0');
+INSERT INTO `t_log` VALUES ('28', 'admin', '置顶公告ID为：2 的公告', '2018-05-16 20:04:19', null, '0');
+INSERT INTO `t_log` VALUES ('29', 'admin', '取消置顶公告ID为：1 的公告', '2018-05-16 20:08:06', null, '0');
+INSERT INTO `t_log` VALUES ('30', 'admin', '取消置顶公告ID为：2 的公告', '2018-05-16 20:08:12', null, '0');
+INSERT INTO `t_log` VALUES ('31', 'admin', '置顶公告ID为：3 的公告', '2018-05-16 20:08:25', null, '0');
+INSERT INTO `t_log` VALUES ('32', 'admin', '置顶公告ID为：1 的公告', '2018-05-16 20:08:30', null, '0');
+INSERT INTO `t_log` VALUES ('33', 'admin', '置顶公告ID为：2 的公告', '2018-05-16 20:08:33', null, '0');
+INSERT INTO `t_log` VALUES ('34', 'admin', '取消置顶公告ID为：2 的公告', '2018-05-16 20:10:50', null, '0');
+INSERT INTO `t_log` VALUES ('35', 'admin', '取消置顶公告ID为：1 的公告', '2018-05-16 20:10:52', null, '0');
+INSERT INTO `t_log` VALUES ('36', 'admin', '取消置顶公告ID为：3 的公告', '2018-05-16 20:10:53', null, '0');
+INSERT INTO `t_log` VALUES ('37', 'admin', '删除公告ID为：1 的公告', '2018-05-16 20:13:30', null, '0');
+INSERT INTO `t_log` VALUES ('38', 'admin', '删除公告ID为：2 的公告', '2018-05-16 20:13:34', null, '0');
 
 -- ----------------------------
 -- Table structure for t_notice
@@ -107,16 +123,22 @@ INSERT INTO `t_log` VALUES ('22', 'admin', '删除用户ID为：6 的用户', '2
 DROP TABLE IF EXISTS `t_notice`;
 CREATE TABLE `t_notice` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `number` char(5) DEFAULT NULL COMMENT '公告编号',
   `content` text NOT NULL COMMENT '公告内容',
+  `top` tinyint(4) NOT NULL DEFAULT '0' COMMENT '置顶功能。1为置顶，0为取消置顶',
   `create_time` char(19) NOT NULL COMMENT '创建记录时间',
   `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
   `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
+  `status` tinyint(4) DEFAULT '0' COMMENT '删除状态，0表示未删除，1表示已删除',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_notice
 -- ----------------------------
+INSERT INTO `t_notice` VALUES ('1', 'N0001', '请把微信姓名改成真实姓名', '0', '2018-05-11 15:52:36', '2018-05-16 20:10:52', '', '0');
+INSERT INTO `t_notice` VALUES ('2', 'N6018', '请准时到达。否则，后果自负！', '0', '2018-05-16 19:25:36', '2018-05-16 20:10:50', '', '0');
+INSERT INTO `t_notice` VALUES ('3', 'N5565', '请保存好发票！', '0', '2018-05-16 19:45:41', '2018-05-16 20:10:53', null, '0');
 
 -- ----------------------------
 -- Table structure for t_oil
@@ -154,10 +176,10 @@ CREATE TABLE `t_order` (
   `oil_type` char(20) NOT NULL COMMENT '油品类型',
   `plate` char(7) NOT NULL COMMENT '车牌号',
   `driver_id` int(11) NOT NULL COMMENT '司机ID',
-  `driver_number` char(20) DEFAULT NULL COMMENT '司机编号',
+  `driver_name` char(20) DEFAULT NULL COMMENT '司机编号',
   `driver_company` char(20) DEFAULT NULL COMMENT '司机隶属公司',
-  `order_status` tinyint(4) NOT NULL COMMENT '订单状态',
-  `order` int(11) NOT NULL COMMENT '排队次序',
+  `order_status` tinyint(4) DEFAULT '3' COMMENT '订单状态。0已装，1装车，2厂内，3厂外',
+  `order_number` int(11) DEFAULT NULL COMMENT '排队次序',
   `create_time` char(19) NOT NULL COMMENT '创建记录时间',
   `update_time` char(19) DEFAULT NULL COMMENT '修改记录时间',
   `delete_time` char(19) DEFAULT NULL COMMENT '删除记录时间',
@@ -167,13 +189,16 @@ CREATE TABLE `t_order` (
   KEY `driver_id` (`driver_id`),
   CONSTRAINT `t_order_ibfk_1` FOREIGN KEY (`oil_id`) REFERENCES `t_oil` (`id`),
   CONSTRAINT `t_order_ibfk_3` FOREIGN KEY (`driver_id`) REFERENCES `t_driver` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_order
 -- ----------------------------
-INSERT INTO `t_order` VALUES ('1', 'O0001', '1', '二甲苯', '芳香烃类', '粤L66666', '1', 'D0001', '一匠科技', '1', '1', '2018-05-11 15:52:36', null, null, '0');
-INSERT INTO `t_order` VALUES ('2', 'O0002', '2', '乙醇', '芳香烃类', '粤L66666', '1', 'D0001', '一匠', '2', '2', '2018-05-11 15:52:36', null, null, '0');
+INSERT INTO `t_order` VALUES ('1', 'O0001', '1', '二甲苯', '芳香烃类', '粤L66666', '1', '赖俊强', '一匠科技', '0', null, '2018-05-11 15:52:36', null, null, '0');
+INSERT INTO `t_order` VALUES ('2', 'O0002', '2', '乙醇', '芳香烃类', '粤L66666', '1', '赖俊强', '一匠', '2', '2', '2018-05-16 15:52:36', null, null, '0');
+INSERT INTO `t_order` VALUES ('3', 'O0003', '5', '丙烯', '烯烃', '粤L88888', '2', '林晓聪', '一匠', '1', '1', '2018-05-13 15:52:36', null, null, '0');
+INSERT INTO `t_order` VALUES ('4', 'O0004', '6', '乙烯', '烯烃', '粤L88888', '2', '林晓聪', '一匠', '3', '3', '2018-05-17 15:52:36', null, null, '0');
+INSERT INTO `t_order` VALUES ('5', 'O0005', '6', '乙烯', '烯烃', '粤L88888', '2', '林晓聪', '一匠', '3', '4', '2018-05-18 15:52:36', null, null, '0');
 
 -- ----------------------------
 -- Table structure for t_role
@@ -199,7 +224,7 @@ CREATE TABLE `t_role` (
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
-INSERT INTO `t_role` VALUES ('31', '超级管理员', '1', '1', '1', '1', '1', '1', '1', '2018-05-14 21:12:58', '2018-05-14 21:16:47', '', '0');
+INSERT INTO `t_role` VALUES ('31', '排队管理员', '1', '0', '0', '0', '0', '0', '0', '2018-05-14 21:12:58', '2018-05-16 18:43:26', '', '0');
 INSERT INTO `t_role` VALUES ('32', '用户管理员', '0', '0', '0', '1', '0', '0', '0', '2018-05-14 21:17:02', null, null, '0');
 
 -- ----------------------------
